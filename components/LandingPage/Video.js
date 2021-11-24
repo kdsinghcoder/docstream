513dad0f4b09d844a37766d97fcaab19f1d4e577
Kunal Kulkarni <56317013+KunalK27@users.noreply.github.com>
2021-11-24 18:12:00 +0530

    Update Video.js

--- components/LandingPage/Video.js
+++ components/LandingPage/Video.js
@@ -1,7 +1,7 @@
 const Video = () => {
     return (
         <section class="text-gray-400 bg-gray-900 body-font">
-            <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+            {/* <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
                 <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
                 <div class="text-center lg:w-2/3 w-full">
                     <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Microdosing synth tattooed vexillologist</h1>
@@ -11,7 +11,7 @@ const Video = () => {
                         <button class="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">Button</button>
                     </div>
                 </div>
-            </div>
+            </div> */}
         </section>
     )
 }
